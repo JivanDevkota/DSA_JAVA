6d1c31ed4253b334bf9761e48186b24913b7486b
Jivan Devkota` <107915294+JivanDevkota@users.noreply.github.com>
2025-06-27 13:58:39 +0545

    second commit

--- src/list/SingleLinkedList.java
+++ src/list/SingleLinkedList.java
@@ -1,2 +1,37 @@
-package list;public class SingleLinkedList {
+package list;
+
+public class SingleLinkedList {
+    private static ListNode head;
+
+    private static class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data=data;
+            this.next=null;
+        }
+
+        public void display(){
+            ListNode current=head;
+            while (current!=null){
+                System.out.print(current.data+"--->");
+                current=current.next;
+            }
+            System.out.println("null");
+        }
+
+        public int length(){
+            if (head==null){
+                return 0;
+            }
+            int count=0;
+            ListNode current=head;   //1->2--3
+            while (current!=null){
+                count++;
+                current=current.next;
+            }
+            return count;
+        }
+    }
 }
